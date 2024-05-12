4db5fcfc0884fe48742428f917556c84e5d31d77
abuba0a <168736244+abuba0a@users.noreply.github.com>
2024-05-12 10:37:10 +0300

    Add files via upload

--- homework7.py
+++ homework7.py
@@ -1,6 +1,23 @@
+# Вариант 1
+
 def print_params():
     print('Params - 1')
     print('Params - 2')
+
+
+print_params()
+print_params()
+print_params()
+
+print('Конец')
+print()
+
+
+# Вариант 2
+
+def print_params():
+    for i in ['Params - 1', 'Params - 2']:
+        print(i)
 
 
 print_params()
